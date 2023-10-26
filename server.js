7d44257d89c9391be0c894925237ace8b33b445a
Pius Samie Henry <145422420+piussamie1byu@users.noreply.github.com>
2023-10-26 23:59:17 +0000

    Added Pius to the team api

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/tristin', (req,res) => {res.send("Hello Tristin")});
 app.get('/Tristin', (req,res) => {res.send("Hello Tristin")});
+app.get('/Pius', (req,res) => {res.send("Hello Pius")});
 
 
 app.listen(port, ()=>console.log("Listening"));

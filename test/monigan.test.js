6a739cde1999a82e5a1abfeca458aaf623f26b74
Monigan Daniel <131489742+monitech1@users.noreply.github.com>
2023-10-31 12:03:25 +0000

    Added monigan.test.js

---
+++ test/monigan.test.js
@@ -0,0 +1,7 @@
+const fetch = require( "node-fetch2" ) ;
+
+it( "Should match my name", async () => { 
+      const response = await fetch("http://localhost:3000/ernest");
+      const name = await response.text();
+      expect(name).toBe("Hello Monigan");
+});

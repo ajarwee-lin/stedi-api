12fdb418896bbc27707cf056f0a828f9b71edbdc
Ernest Li <li23002@byui.edu>
2023-11-02 20:07:55 +0800

    W08 Scrum Team Activity

--- test/ernest.test.js
+++ test/ernest.test.js
@@ -2,6 +2,6 @@ const fetch = require( "node-fetch2" ) ;
 
 it( "Should match my name", async () => { 
       const response = await fetch("http://localhost:3000/ernest");
-      const name = await response.text();
-      expect(name).toBe("Hello Ernest");
+      const myName = await response.text();
+      expect(myName).toBe("Hello Ernest");
 });

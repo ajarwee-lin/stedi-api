98e1ed6f91ea7fd7d69a9a67730f36c459a7662c
Ernest Li <li23002@byui.edu>
2023-11-03 07:51:29 +0800

    Added monigan.test.js

--- test/monigan.test.js
+++ test/monigan.test.js
@@ -1,7 +1,7 @@
 const fetch = require( "node-fetch2" ) ;
 
 it( "Should match my name", async () => { 
-      const response = await fetch("http://localhost:3000/ernest");
+      const response = await fetch("http://localhost:3000/monigan");
       const name = await response.text();
       expect(name).toBe("Hello Monigan");
 });

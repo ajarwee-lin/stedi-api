c20bd37ad4d5bbaac294822e59fee5dfcd06ac7e
Ernest Li <li23002@byui.edu>
2023-11-03 08:53:51 +0800

    Update jarwee.test.js

--- test/jarwee.test.js
+++ test/jarwee.test.js
@@ -1,7 +1,7 @@
 const fetch = require( "node-fetch2" ) ;
 
 it( "Should match my name", async () => { 
-      const response = await fetch("http://localhost:3000/ernest");
+      const response = await fetch("http://localhost:3000/jarwee");
       const myName = await response.text();
       expect(myName).toBe("Hello Jarwee");
 });

9f90be66c7e16143bfd0eca3a63de550c352fda1
ajarwee-lin <122073281+ajarwee-lin@users.noreply.github.com>
2023-11-02 18:46:12 -0600

    Update jarwee.test.js

--- test/jarwee.test.js
+++ test/jarwee.test.js
@@ -1,1 +1,7 @@
+const fetch = require( "node-fetch2" ) ;
 
+it( "Should match my name", async () => { 
+      const response = await fetch("http://localhost:3000/ernest");
+      const myName = await response.text();
+      expect(myName).toBe("Hello Jarwee");
+});

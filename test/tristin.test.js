4434a4370b818d7ee5838a4ce1f5fc822322230b
TristinParker <gidgidonihah@byui.edu>
2023-11-02 18:40:26 -0600

    Tristin Commit

---
+++ test/tristin.test.js
@@ -0,0 +1,7 @@
+const fetch = require( "node-fetch2" ) ;
+
+it( "Should match my name", async () => { 
+      const response = await fetch("http://localhost:3000/tristin");
+      const myName = await response.text();
+      expect(myName).toBe("Hello Tristin");
+});
